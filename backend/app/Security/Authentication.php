ee85b1f170d34d4a511f1a2753be2c2debd57710
Luis Pacheco <laps1308@gmail.com>
2024-09-24 22:07:15 -0500

    modified authentication to get key -currently get empty

--- backend/app/Security/Authentication.php
+++ backend/app/Security/Authentication.php
@@ -9,6 +9,7 @@ use \stdClass;
 
 class Authentication{
     const string hash = 'HS512';
+    const string key = "5c7cfd2da9de3dc0fe0982ee2feaa1344bb120c6";
     public static function jwt(string $email,int $id):string{
 
         $creationDate = new \DateTime('now');
@@ -21,14 +22,17 @@ class Authentication{
             'exp' => $expiration->getTimestamp(),
             'id'  => $id
         ];
-        $key =(string) env('JWT_KEY');
-        $jwt = JWT::encode($payload,$key ,self::hash);
+        //$key =(string) env('JWT_KEY');
+        $jwt = JWT::encode($payload,self::key ,self::hash);
         return $jwt;
     }
     public static function validate(string $jwt):Array{
         $stdClass =new stdClass();
-        $key = (string) env('JWT_KEY');
-        $decode = JWT::decode($jwt, new Key($key, self::hash),$stdClass);
+
+        /
+        //$key = (string) env('JWT_KEY');
+        
+        $decode = JWT::decode($jwt, new Key(self::key, self::hash),$stdClass);
         return (Array) $decode;
     }
 }
